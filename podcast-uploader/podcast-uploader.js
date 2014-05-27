19e4a67fdbeee7133fd899d58a0664850acd9dd8
Ray Nicholus <rnicholus@users.noreply.github.com>
2014-05-27 10:15:18 -0500

    various adjustments to improve code
    1. Wrap the code in a IIFE, passing `jQuery` into the function via the `$` param to allow use of the dollar sign alias in all cases.
    2. Removed call to `ready` in favor of shorter alias function.
    3. Remove dependence on specific CSS classes used by Fine Uploader.  Used an API method that returns the container element for a specific file instead.
    4. Made use of `getKey` to create the public URL for the stored object.

--- podcast-uploader/podcast-uploader.js
+++ podcast-uploader/podcast-uploader.js
@@ -1,22 +1,25 @@
-/* Note for artile, I had to add in and call this using jQuery instead of $ */
-jQuery(document).ready( function() {
-	jQuery('#fine-uploader').fineUploaderS3({
-		debug: true,
-		request: {
-			endpoint: "${ENDPOINT_HERE}",
-			accessKey: "${ACCESS_KEY}"
-		},
-		signature: {
-			endpoint: "/s3/fine-uploader-server.php"
-		},
-		objectProperties: {
-			acl: "public-read",
-			key: function(fileId) {
-				return "podcasts/" + jQuery('#fine-uploader').fineUploaderS3('getName', fileId);
+(function($) {
+	$(function() {
+		$('#fine-uploader').fineUploaderS3({
+			debug: true,
+			request: {
+				endpoint: "${ENDPOINT_HERE}",
+				accessKey: "${ACCESS_KEY}"
+			},
+			signature: {
+				endpoint: "/s3/fine-uploader-server.php"
+			},
+			objectProperties: {
+				acl: "public-read",
+				key: function(fileId) {
+					return "podcasts/" + this.getName(fileId);
+				}
 			}
-		}
-	}).on("complete", function( event, id, name, responseJson, xhr ) {
-        var podcastUrl = ${S3_BUCKET_URL} + name;
-        jQuery('.qq-file-id-' + id).append('<div><p><strong>File Location:</strong> <a href="' + podcastUrl + '">' + podcastUrl + '</a></p></div>');
+		}).on("complete", function( event, id, name, responseJson, xhr ) {
+			var key = this.fineUploaderS3("getKey", id),
+	        	    podcastUrl = "${ENDPOINT_HERE}" + key,
+	        	    $fileContainer = this.fineUploader("getItemByFileId", id);
+	        	$fileContainer.append('<div><p><strong>File Location:</strong> <a href="' + podcastUrl + '">' + podcastUrl + '</a></p></div>');
+		});
 	});
-});
+}(jQuery));
